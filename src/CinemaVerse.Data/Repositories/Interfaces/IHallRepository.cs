9d1e90eda20baa27a17da77dca760f972b5cd7b9
Omar Abouelkheir <177960935+OmarAbouelkheirr@users.noreply.github.com>
2025-12-12 14:25:06 +0200

    feat(DataLayer): implement repositories for Halls, Movies, and Branches with full implementations

---
+++ src/CinemaVerse.Data/Repositories/Interfaces/IHallRepository.cs
@@ -0,0 +1,16 @@
+﻿using CinemaVerse.Data.Models;
+
+namespace CinemaVerse.Data.Repositories.Interfaces
+{
+    public interface IHallRepository : IRepository<Hall>
+    {
+        Task<Hall?> GetByIdWithDetailsAsync(int id);
+        Task<IEnumerable<Hall>> GetHallsWithDetailsByBranchAsync(int branchId);
+        Task<IEnumerable<Hall>> GetHallsByBranchIdAsync(int branchId);
+        Task<bool> IsHallNumberExistsAsync(int branchId, string hallNumber);
+        Task<IEnumerable<Hall>> GetAvailableHallsByBranchIdAsync(int branchId);
+
+        //Task<IEnumerable<Hall>> GetAvailableHallsAsync();
+        //Task<IEnumerable<Hall>> GetHallsWithSeatsAsync();
+    }
+}
---
+++ src/CinemaVerse.Data/Repositories/Interfaces/IMovieRepository.cs
@@ -0,0 +1,11 @@
+﻿using CinemaVerse.Data.Models;
+
+namespace CinemaVerse.Data.Repositories.Interfaces
+{
+    public interface IMovieRepository : IRepository<Movie>
+    {
+        Task<Movie?> GetMovieWithDetailsByIdAsync(int id);
+        Task<IEnumerable<Movie>> GetByGenreAsync(int genreId);
+        Task<IEnumerable<Movie>> SearchByNameAsync(string keyword);
+    }
+}
