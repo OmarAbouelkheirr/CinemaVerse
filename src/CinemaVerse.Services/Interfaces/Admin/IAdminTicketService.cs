26ee0c0d4aa9b1bbd4db8873e1d8fd0d31522871
Omar Abouelkheir <177960935+OmarAbouelkheirr@users.noreply.github.com>
2026-01-26 04:27:46 +0200

    feat: Add IAdminTicketService interface
    - Added interface for AdminTicketService
    - Defined GetAllTicketsAsync method signature

---
+++ src/CinemaVerse.Services/Interfaces/Admin/IAdminTicketService.cs
@@ -0,0 +1,12 @@
+﻿using CinemaVerse.Services.DTOs.AdminFlow.AdminTicket.Requests;
+using CinemaVerse.Services.DTOs.AdminFlow.AdminTicket.Response;
+using CinemaVerse.Services.DTOs.Common;
+
+namespace CinemaVerse.Services.Interfaces.Admin
+{
+    public interface IAdminTicketService
+    {
+        Task<PagedResultDto<AdminTicketDetailsDto>> GetAllTicketsAsync(AdminTicketFilterDto filter);
+
+    }
+}
